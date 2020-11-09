499f46649eb422788c4aaa88e41c1bc6890d392e
Reuven Podmazo <reuven@enigma.co>
2020-11-09 13:34:28 +0200

    removed unused import

--- packages/crypto/src/sign.rs
+++ packages/crypto/src/sign.rs
@@ -1,5 +1,3 @@
-use secp256k1;
-
 pub fn pubkey(priv_key: &[u8; 32]) -> secp256k1::PublicKey {
     let pk = secp256k1::SecretKey::parse(priv_key).unwrap();
     secp256k1::PublicKey::from_secret_key(&pk)
