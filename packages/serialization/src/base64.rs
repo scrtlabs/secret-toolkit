77a9d1f7ae5d2ebd5a195cc656359fca031ecd48
Reuven Podmazo <reuven@enigma.co>
2021-09-29 19:40:17 +0300

    cleaned up imports

--- packages/serialization/src/base64.rs
+++ packages/serialization/src/base64.rs
@@ -5,6 +5,8 @@ use serde::{de, ser};
 
 use cosmwasm_std::Binary;
 
+use crate::Serde;
+
 /// Alias of `cosmwasm_std::Binary` for better naming
 pub type Base64 = Binary;
 
@@ -14,7 +16,7 @@ pub type Base64 = Binary;
 /// example in the `msg` field of the `Receive` interface, to remove the
 /// boilerplate of serializing or deserializing the `Binary` to the relevant
 /// type `T`.
-pub struct Base64Of<S: crate::Serde, T> {
+pub struct Base64Of<S: Serde, T> {
     // This is pub so that users can easily unwrap this if needed,
     // or just swap the entire instance.
     pub inner: T,
@@ -27,7 +29,7 @@ pub type Base64JsonOf<T> = Base64Of<crate::Json, T>;
 #[cfg(feature = "bincode2")]
 pub type Base64Bincode2Of<T> = Base64Of<crate::Bincode2, T>;
 
-impl<S: crate::Serde, T> From<T> for Base64Of<S, T> {
+impl<S: Serde, T> From<T> for Base64Of<S, T> {
     fn from(other: T) -> Self {
         Self {
             inner: other,
@@ -36,20 +38,20 @@ impl<S: crate::Serde, T> From<T> for Base64Of<S, T> {
     }
 }
 
-impl<S: crate::Serde, T> std::ops::Deref for Base64Of<S, T> {
+impl<S: Serde, T> std::ops::Deref for Base64Of<S, T> {
     type Target = T;
     fn deref(&self) -> &Self::Target {
         &self.inner
     }
 }
 
-impl<S: crate::Serde, T> std::ops::DerefMut for Base64Of<S, T> {
+impl<S: Serde, T> std::ops::DerefMut for Base64Of<S, T> {
     fn deref_mut(&mut self) -> &mut Self::Target {
         &mut self.inner
     }
 }
 
-impl<Ser: crate::Serde, T: ser::Serialize> ser::Serialize for Base64Of<Ser, T> {
+impl<Ser: Serde, T: ser::Serialize> ser::Serialize for Base64Of<Ser, T> {
     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
     where
         S: ser::Serializer,
@@ -62,9 +64,7 @@ impl<Ser: crate::Serde, T: ser::Serialize> ser::Serialize for Base64Of<Ser, T> {
     }
 }
 
-impl<'de, S: crate::Serde, T: for<'des> de::Deserialize<'des>> de::Deserialize<'de>
-    for Base64Of<S, T>
-{
+impl<'de, S: Serde, T: for<'des> de::Deserialize<'des>> de::Deserialize<'de> for Base64Of<S, T> {
     fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
     where
         D: de::Deserializer<'de>,
@@ -73,12 +73,12 @@ impl<'de, S: crate::Serde, T: for<'des> de::Deserialize<'des>> de::Deserialize<'
     }
 }
 
-struct Base64TVisitor<S: crate::Serde, T> {
+struct Base64TVisitor<S: Serde, T> {
     inner: PhantomData<T>,
     ser: PhantomData<S>,
 }
 
-impl<S: crate::Serde, T> Base64TVisitor<S, T> {
+impl<S: Serde, T> Base64TVisitor<S, T> {
     fn new() -> Self {
         Self {
             inner: PhantomData,
@@ -87,9 +87,7 @@ impl<S: crate::Serde, T> Base64TVisitor<S, T> {
     }
 }
 
-impl<'de, S: crate::Serde, T: for<'des> de::Deserialize<'des>> de::Visitor<'de>
-    for Base64TVisitor<S, T>
-{
+impl<'de, S: Serde, T: for<'des> de::Deserialize<'des>> de::Visitor<'de> for Base64TVisitor<S, T> {
     type Value = Base64Of<S, T>;
 
     fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
@@ -119,15 +117,18 @@ mod passthrough_impls {
     use std::hash::{Hash, Hasher};
     use std::marker::PhantomData;
 
-    use schemars::JsonSchema;
-
-    use super::Base64Of;
-    use cosmwasm_std::Binary;
     use schemars::gen::SchemaGenerator;
     use schemars::schema::Schema;
+    use schemars::JsonSchema;
+
+    use cosmwasm_std::Binary;
+
+    use crate::Serde;
+
+    use super::Base64Of;
 
     // Clone
-    impl<S: crate::Serde, T: Clone> Clone for Base64Of<S, T> {
+    impl<S: Serde, T: Clone> Clone for Base64Of<S, T> {
         fn clone(&self) -> Self {
             Self {
                 inner: self.inner.clone(),
@@ -137,32 +138,30 @@ mod passthrough_impls {
     }
 
     // Copy
-    impl<S: crate::Serde, T: Copy> Copy for Base64Of<S, T> {}
+    impl<S: Serde, T: Copy> Copy for Base64Of<S, T> {}
 
     // Debug
-    impl<S: crate::Serde, T: Debug> Debug for Base64Of<S, T> {
+    impl<S: Serde, T: Debug> Debug for Base64Of<S, T> {
         fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
             self.inner.fmt(f)
         }
     }
 
     // Display
-    impl<S: crate::Serde, T: Display> Display for Base64Of<S, T> {
+    impl<S: Serde, T: Display> Display for Base64Of<S, T> {
         fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
             self.inner.fmt(f)
         }
     }
 
     // PartialEq
-    impl<S: crate::Serde, S2: crate::Serde, T: PartialEq> PartialEq<Base64Of<S2, T>>
-        for Base64Of<S, T>
-    {
+    impl<S: Serde, S2: Serde, T: PartialEq> PartialEq<Base64Of<S2, T>> for Base64Of<S, T> {
         fn eq(&self, other: &Base64Of<S2, T>) -> bool {
             self.inner.eq(&other.inner)
         }
     }
 
-    impl<S: crate::Serde, T: PartialEq> PartialEq<T> for Base64Of<S, T> {
+    impl<S: Serde, T: PartialEq> PartialEq<T> for Base64Of<S, T> {
         fn eq(&self, other: &T) -> bool {
             self.inner.eq(other)
         }
@@ -175,18 +174,16 @@ mod passthrough_impls {
     // covers that case already. Basically it doesn't understand that S1 and S2
     // _can_ be the same type.
     //
-    // impl<S: crate::Serde, T: Eq> Eq for Base64Of<S, T> {}
+    // impl<S: Serde, T: Eq> Eq for Base64Of<S, T> {}
 
     // PartialOrd
-    impl<S: crate::Serde, S2: crate::Serde, T: PartialOrd> PartialOrd<Base64Of<S2, T>>
-        for Base64Of<S, T>
-    {
+    impl<S: Serde, S2: Serde, T: PartialOrd> PartialOrd<Base64Of<S2, T>> for Base64Of<S, T> {
         fn partial_cmp(&self, other: &Base64Of<S2, T>) -> Option<Ordering> {
             self.inner.partial_cmp(&other.inner)
         }
     }
 
-    impl<S: crate::Serde, T: PartialOrd> PartialOrd<T> for Base64Of<S, T> {
+    impl<S: Serde, T: PartialOrd> PartialOrd<T> for Base64Of<S, T> {
         fn partial_cmp(&self, other: &T) -> Option<Ordering> {
             self.inner.partial_cmp(other)
         }
@@ -196,14 +193,14 @@ mod passthrough_impls {
     // This can not be implemented for the same reason that `Eq` can't be implemented.
 
     // Hash
-    impl<S: crate::Serde, T: Hash> Hash for Base64Of<S, T> {
+    impl<S: Serde, T: Hash> Hash for Base64Of<S, T> {
         fn hash<H: Hasher>(&self, state: &mut H) {
             self.inner.hash(state)
         }
     }
 
     // Default
-    impl<S: crate::Serde, T: Default> Default for Base64Of<S, T> {
+    impl<S: Serde, T: Default> Default for Base64Of<S, T> {
         fn default() -> Self {
             Self {
                 inner: T::default(),
@@ -213,7 +210,7 @@ mod passthrough_impls {
     }
 
     // JsonSchema
-    impl<S: crate::Serde, T: JsonSchema> JsonSchema for Base64Of<S, T> {
+    impl<S: Serde, T: JsonSchema> JsonSchema for Base64Of<S, T> {
         fn schema_name() -> String {
             Binary::schema_name()
         }
@@ -226,10 +223,10 @@ mod passthrough_impls {
 
 #[cfg(test)]
 mod test {
-    use cosmwasm_schema::schema_for;
     use schemars::JsonSchema;
     use serde::{Deserialize, Serialize};
 
+    use cosmwasm_schema::schema_for;
     use cosmwasm_std::{Binary, StdResult};
 
     use crate::base64::Base64JsonOf;
