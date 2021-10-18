e9547963483bfc9f3d52a9bad517c413e65d1c66
Assaf Morami <assaf.morami@gmail.com>
2021-10-18 21:26:22 +0300

    RevokedPemits don't hardcode the storage key prefix

--- packages/permit/src/state.rs
+++ packages/permit/src/state.rs
@@ -1,22 +1,26 @@
 use cosmwasm_std::{HumanAddr, Storage};
 
-pub const PREFIX_REVOKED_PERMITS: &str = "revoked_permits";
-
 pub struct RevokedPemits;
 
 impl RevokedPemits {
     pub fn is_permit_revoked(
         storgae: &dyn Storage,
+        storage_prefix: &str,
         account: &HumanAddr,
         permit_name: &str,
     ) -> bool {
-        let storage_key = PREFIX_REVOKED_PERMITS.to_string() + &account.to_string() + permit_name;
+        let storage_key = storage_prefix.to_string() + &account.to_string() + permit_name;
 
         storgae.get(storage_key.as_bytes()).is_some()
     }
 
-    pub fn revoke_permit(storage: &mut dyn Storage, account: &HumanAddr, permit_name: &str) {
-        let storage_key = PREFIX_REVOKED_PERMITS.to_string() + &account.to_string() + permit_name;
+    pub fn revoke_permit(
+        storage: &mut dyn Storage,
+        storage_prefix: &str,
+        account: &HumanAddr,
+        permit_name: &str,
+    ) {
+        let storage_key = storage_prefix.to_string() + &account.to_string() + permit_name;
 
         storage.set(storage_key.as_bytes(), &[])
     }
