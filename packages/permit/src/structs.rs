52df67c2becf98510fa55bdad5edf0e2dd11f5a6
Assaf Morami <assaf.morami@gmail.com>
2021-10-19 12:49:22 +0300

    Permit::Permission more verbose docs for SNIP-20

--- packages/permit/src/structs.rs
+++ packages/permit/src/structs.rs
@@ -156,18 +156,18 @@ impl PermitContent {
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub enum Permission {
-    /// Allowance for SNIP-20
+    /// Allowance for SNIP-20 - Permission to query allowance of the owner & spender
     Allowance,
-    /// Balance for SNIP-20
+    /// Balance for SNIP-20 - Permission to query balance
     Balance,
-    /// History for SNIP-20
+    /// History for SNIP-20 - Permission to query transfer_history & transaction_hisotry
     History,
-    /// Owner permission indicates that the bearer of this permit should be granted all 
-    /// the access of the creator/signer of the permit.  SNIP-721 uses this to grant 
+    /// Owner permission indicates that the bearer of this permit should be granted all
+    /// the access of the creator/signer of the permit.  SNIP-721 uses this to grant
     /// viewing access to all data that the permit creator owns and is whitelisted for.
-    /// For SNIP-721 use, a permit with Owner permission should NEVER be given to 
+    /// For SNIP-721 use, a permit with Owner permission should NEVER be given to
     /// anyone else.  If someone wants to share private data, they should whitelist
-    /// the address they want to share with via a SetWhitelistedApproval tx, and that 
+    /// the address they want to share with via a SetWhitelistedApproval tx, and that
     /// address will view the data by creating their own permit with Owner permission
     Owner,
 }
