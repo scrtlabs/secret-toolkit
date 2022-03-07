f1a8773a5eb7321ca71482f3e8109e9d2125ff2c
Reuven Podmazo <reuven@enigma.co>
2022-03-07 23:17:57 +0200

    added internal handling of viewing key store randomness seed

--- packages/viewing_key/src/lib.rs
+++ packages/viewing_key/src/lib.rs
@@ -9,6 +9,7 @@ use secret_toolkit_crypto::{sha_256, Prng, SHA256_HASH_SIZE};
 
 pub const VIEWING_KEY_SIZE: usize = SHA256_HASH_SIZE;
 pub const VIEWING_KEY_PREFIX: &str = "api_key_";
+const SEED_KEY: &[u8] = b"::seed";
 
 /// This is the default implementation of the viewing key store, using the "viewing_keys"
 /// storage prefix.
@@ -27,20 +28,36 @@ impl ViewingKeyStore for ViewingKey {
 pub trait ViewingKeyStore {
     const STORAGE_KEY: &'static [u8];
 
+    /// Set the initial prng seed for the store
+    fn set_seed<S: Storage>(storage: &mut S, seed: &[u8]) {
+        let mut seed_key = Vec::new();
+        seed_key.extend_from_slice(Self::STORAGE_KEY);
+        seed_key.extend_from_slice(b"::seed");
+
+        storage.set(&seed_key, seed)
+    }
+
     /// Create a new viewing key, save it to storage, and return it.
     ///
-    /// The random seed and entropy should be managed externally
+    /// The random entropy should be provided from some external source, such as the user.
     fn create<S: Storage>(
         storage: &mut S,
         env: &Env,
         account: &HumanAddr,
-        seed: &[u8],
         entropy: &[u8],
     ) -> String {
-        let viewing_key = new_viewing_key(env, seed, entropy);
+        let mut seed_key = Vec::with_capacity(Self::STORAGE_KEY.len() + SEED_KEY.len());
+        seed_key.extend_from_slice(Self::STORAGE_KEY);
+        seed_key.extend_from_slice(SEED_KEY);
+        let seed = storage.get(&seed_key).unwrap_or_default();
+
+        let (viewing_key, next_seed) = new_viewing_key(env, &seed, entropy);
         let mut balance_store = PrefixedStorage::new(Self::STORAGE_KEY, storage);
         let hashed_key = sha_256(viewing_key.as_bytes());
         balance_store.set(account.as_str().as_bytes(), &hashed_key);
+
+        storage.set(&seed_key, &next_seed);
+
         viewing_key
     }
 
@@ -74,7 +91,7 @@ pub trait ViewingKeyStore {
     }
 }
 
-fn new_viewing_key(env: &Env, seed: &[u8], entropy: &[u8]) -> String {
+fn new_viewing_key(env: &Env, seed: &[u8], entropy: &[u8]) -> (String, [u8; 32]) {
     // 16 here represents the lengths in bytes of the block height and time.
     let entropy_len = 16 + env.message.sender.len() + entropy.len();
     let mut rng_entropy = Vec::with_capacity(entropy_len);
@@ -89,7 +106,8 @@ fn new_viewing_key(env: &Env, seed: &[u8], entropy: &[u8]) -> String {
 
     let key = sha_256(&rand_slice);
 
-    VIEWING_KEY_PREFIX.to_string() + &base64::encode(&key)
+    let viewing_key = VIEWING_KEY_PREFIX.to_string() + &base64::encode(&key);
+    (viewing_key, rand_slice)
 }
 
 fn ct_slice_compare(s1: &[u8], s2: &[u8]) -> bool {
@@ -113,11 +131,15 @@ mod tests {
         let result = ViewingKey::check(&deps.storage, &account, "fake key");
         assert_eq!(result, Err(StdError::unauthorized()));
 
-        let viewing_key =
-            ViewingKey::create(&mut deps.storage, &env, &account, b"seed", b"entropy");
+        ViewingKey::set_seed(&mut deps.storage, b"seed");
+        let viewing_key = ViewingKey::create(&mut deps.storage, &env, &account, b"entropy");
 
         let result = ViewingKey::check(&deps.storage, &account, &viewing_key);
         assert_eq!(result, Ok(()));
+
+        // Create a key with the same entropy. Check that it's different
+        let viewing_key_2 = ViewingKey::create(&mut deps.storage, &env, &account, b"entropy");
+        assert_ne!(viewing_key, viewing_key_2);
 
         // VK set to another key:
         let result = ViewingKey::check(&deps.storage, &account, "fake key");
