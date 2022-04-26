8be6cf3021e751df93a1302b7aefe702a12331f4
Ben Adams <darwinzero@gmail.com>
2022-04-26 20:08:48 +1200

    update permit for new api

--- packages/permit/src/funcs.rs
+++ packages/permit/src/funcs.rs
@@ -2,7 +2,6 @@ use cosmwasm_std::{
     to_binary, Api, Binary, CanonicalAddr, Extern, HumanAddr, Querier, StdError, StdResult, Storage,
 };
 use ripemd160::{Digest, Ripemd160};
-use secp256k1::Secp256k1;
 
 use crate::{Permit, RevokedPermits, SignedPermit};
 
@@ -41,32 +40,20 @@ pub fn validate<S: Storage, A: Api, Q: Querier>(
         )));
     }
 
-    // Validate signature, reference: https://github.com/enigmampc/SecretNetwork/blob/f591ed0cb3af28608df3bf19d6cfb733cca48100/cosmwasm/packages/wasmi-runtime/src/crypto/secp256k1.rs#L49-L82
+    // Verify signature
     let signed_bytes = to_binary(&SignedPermit::from_params(&permit.params))?;
     let signed_bytes_hash = secret_toolkit_crypto::sha_256(signed_bytes.as_slice());
-    let secp256k1_msg = secp256k1::Message::from_slice(&signed_bytes_hash).map_err(|err| {
-        StdError::generic_err(format!(
-            "Failed to create a secp256k1 message from signed_bytes: {:?}",
-            err
-        ))
+    let validated = deps.api.secp256k1_verify(
+        signed_bytes_hash.as_slice(),
+        permit.signature.signature.0.as_slice(),
+        pubkey.as_slice(),
+    ).map_err(|err| {
+        StdError::generic_err(format!("{:?}", err))
     })?;
 
-    let secp256k1_verifier = Secp256k1::verification_only();
-
-    let secp256k1_signature = secp256k1::Signature::from_compact(&permit.signature.signature.0)
-        .map_err(|err| StdError::generic_err(format!("Malformed signature: {:?}", err)))?;
-    let secp256k1_pubkey = secp256k1::PublicKey::from_slice(pubkey.0.as_slice())
-        .map_err(|err| StdError::generic_err(format!("Malformed pubkey: {:?}", err)))?;
-
-    secp256k1_verifier
-        .verify(&secp256k1_msg, &secp256k1_signature, &secp256k1_pubkey)
-        .map_err(|err| {
-            StdError::generic_err(format!(
-                "Failed to verify signatures for the given permit: {:?}",
-                err
-            ))
-        })?;
-
+    if !validated {
+        return Err(StdError::generic_err(format!("Permit {:?} was not validated", permit_name)));
+    }
     Ok(account)
 }
 
