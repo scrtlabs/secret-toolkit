c5fffc12f54357301fdbcb4f83f2ea7f995afb9f
Reuven Podmazo <reuven@enigma.co>
2022-05-09 11:34:17 +0300

    fixed issue with incubator imports

--- packages/incubator/src/lib.rs
+++ packages/incubator/src/lib.rs
@@ -1,8 +1,9 @@
 #[cfg(feature = "cashmap")]
 pub mod cashmap;
+#[cfg(feature = "cashmap")]
+pub use cashmap::{CashMap, ReadOnlyCashMap};
 
 #[cfg(feature = "generational-store")]
 pub mod generational_store;
-
-pub use cashmap::{CashMap, ReadOnlyCashMap};
+#[cfg(feature = "generational-store")]
 pub use generational_store::{GenerationalStore, GenerationalStoreMut};
