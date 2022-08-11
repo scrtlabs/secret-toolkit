5d160b3dcf4734fa0654a4a921281a43677b8487
TomL94 <langer.tom7@gmail.com>
2022-08-11 14:50:19 +0300

    Added `Code` struct to `types.rs`

--- packages/utils/src/types.rs
+++ packages/utils/src/types.rs
@@ -6,3 +6,9 @@ pub struct Contract {
     pub address: String,
     pub hash: String,
 }
+
+#[derive(Serialize, Deserialize, Eq, PartialEq, Debug, Clone, JsonSchema)]
+pub struct Code {
+    pub code_id: u64,
+    pub hash: String,
+}
