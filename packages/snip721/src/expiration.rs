8f49e23930a302765476b4353454c419195fe4d1
Eshel <eshel@scrtlabs.com>
2023-01-30 01:25:14 +0200

    two write! macros also trigger clippy

--- packages/snip721/src/expiration.rs
+++ packages/snip721/src/expiration.rs
@@ -19,8 +19,8 @@ pub enum Expiration {
 impl fmt::Display for Expiration {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
-            Expiration::AtHeight(height) => write!(f, "expiration height: {}", height),
-            Expiration::AtTime(time) => write!(f, "expiration time: {}", time),
+            Expiration::AtHeight(height) => write!(f, "expiration height: {height}"),
+            Expiration::AtTime(time) => write!(f, "expiration time: {time}"),
             Expiration::Never => write!(f, "expiration: never"),
         }
     }
