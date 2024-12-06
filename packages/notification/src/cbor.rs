8602cb64d308368592f5afe9289d61a1a7b85717
Blake Regalia <blake.regalia@gmail.com>
2024-12-06 15:21:12 -0800

    fix: revert visibility modifier

--- packages/notification/src/cbor.rs
+++ packages/notification/src/cbor.rs
@@ -51,7 +51,7 @@ pub trait EncoderExt {
 	fn ext_timestamp(&mut self, value: u64) -> StdResult<&mut Self>;
 }
 
-pub impl<T: cbor_encode::Write> EncoderExt for Encoder<T> {
+impl<T: cbor_encode::Write> EncoderExt for Encoder<T> {
 	#[inline]
 	fn ext_tag(&mut self, tag: cbor_data::IanaTag) -> StdResult<&mut Self> {
 		 self
