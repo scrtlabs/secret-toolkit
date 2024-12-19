76bf3a254d52fc8fa318cff3e1f0d2846eecb93d
Ben Adams <darwinzero@gmail.com>
2024-12-19 19:41:10 +1300

    add data time string to seconds

--- packages/utils/src/datetime.rs
+++ packages/utils/src/datetime.rs
@@ -22,6 +22,30 @@ pub fn iso8601_utc0_to_timestamp(iso8601_str: &str) -> StdResult<Timestamp> {
     }
 }
 
+/// Converts an ISO 8601 date-time string in Zulu (UTC+0) format to seconds.
+/// 
+/// String format: {YYYY}-{MM}-{DD}T{hh}:{mm}:{ss}.{uuu}Z
+/// 
+/// * `iso8601_str` - The ISO 8601 date-time string to convert.
+/// 
+/// Returns StdResult<u64>
+pub fn iso8601_utc0_to_seconds(iso8601_str: &str) -> StdResult<u64> {
+    let Ok(datetime) = iso8601_str.parse::<DateTime<Utc>>() else {
+        return Err(StdError::generic_err("ISO 8601 string could not be parsed"));
+    };
+
+    // Verify the timezone is UTC (Zulu time)
+    if iso8601_str.ends_with("Z") {
+        let seconds = datetime.timestamp();
+        if seconds < 0 {
+            return Err(StdError::generic_err("Date time before January 1, 1970 0:00:00 UTC not supported"))
+        }
+        Ok(seconds as u64)
+    } else {
+        Err(StdError::generic_err("ISO 8601 string not in Zulu (UTC+0)"))
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::iso8601_utc0_to_timestamp;
@@ -30,6 +54,7 @@ mod tests {
     fn test_iso8601_utc0_to_timestamp() {
         let dt_string = "2024-12-17T16:59:00.000Z";
         let timestamp = iso8601_utc0_to_timestamp(dt_string).unwrap();
+        println!("{:?}", timestamp);
         assert_eq!(timestamp.nanos(), 1734454740000000000);
 
         let dt_string = "2024-12-17T16:59:00.000";
